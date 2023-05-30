060a15b5527528befb2f1616fba1173bc9b21583
PeterAminu <Peteraminu@yahoo.com>
2023-05-30 19:04:33 +0100

    Semicolon Assignment

---
+++ AirConditioner.java
@@ -0,0 +1,41 @@
+public class AirConditioner {
+
+	private boolean isOn;
+
+	private String productName;
+
+	private int temperature;
+
+     public AirConditioner(boolean isOn, String productName, int temperature) {
+     		this.isOn = isOn;
+     		this.productName = productName;
+     		this.temperature = temperature;
+}
+	public void decreaseTemperature() {
+		if(isOn && temperature > 0);
+		temperature++;
+}
+	public String getProductName() {
+		return productName;
+} 
+	public int getTemperature() {
+		return temperature;
+}
+	public void increaseTemperature() {
+		if (isOn && temperature > 30);
+		temperature--;
+}
+	public boolean isOn() {
+		return isOn;
+}
+	public void setisOn(boolean isOn) {
+		this.isOn = isOn;
+}
+	public void setProductName(String productName){
+		this.productName = productName;
+}
+	public void setTemperature(int temperature){
+		this.temperature = temperature;
+}
+
+}
